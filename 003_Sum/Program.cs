ce060efb7362215d9cf6e06ebe7bcff0764ba4c1
Alexander Lemport <lemportpro@mail.ru>
2023-04-25 19:23:05 +0300

    Change math project: add randomize

--- 003_Sum/Program.cs
+++ 003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 12;
-int numberB = 6;
-Console.WriteLine(numberA / numberB);
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
