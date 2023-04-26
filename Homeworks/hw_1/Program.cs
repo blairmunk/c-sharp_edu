52c3808390c61ddb6f414a77e7042b830ab396aa
Alexander Lemport <lemportpro@mail.ru>
2023-04-26 11:23:17 +0300

    Solved the task #4

--- Homeworks/hw_1/Program.cs
+++ Homeworks/hw_1/Program.cs
@@ -19,14 +19,23 @@
 
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
+// 2, 3, 7 -> 7; 44 5 78 -> 78; 22 3 9 -> 22
 
-Console.Write("Input number 1: ");
+Console.WriteLine("Input number three numbers: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input number 2: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    if (num1 > num3) Console.WriteLine($"Max number is {num1}.");
+    else Console.WriteLine($"Max number is {num3}.");
+}
+else
+{
+    if (num2 > num3) Console.WriteLine($"Max number is {num2}.");
+    else Console.WriteLine($"Max number is {num3}.");
+}
 
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
