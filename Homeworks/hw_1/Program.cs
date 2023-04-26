afd9b0555fb275ab71031b57f0ae848db67e1d78
Alexander Lemport <lemportpro@mail.ru>
2023-04-26 11:31:22 +0300

    Solved the task #6

--- Homeworks/hw_1/Program.cs
+++ Homeworks/hw_1/Program.cs
@@ -21,28 +21,37 @@
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 // 2, 3, 7 -> 7; 44 5 78 -> 78; 22 3 9 -> 22
 
-Console.WriteLine("Input number three numbers: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = Convert.ToInt32(Console.ReadLine());
-int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input three numbers: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int num3 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
-{
-    if (num1 > num3) Console.WriteLine($"Max number is {num1}.");
-    else Console.WriteLine($"Max number is {num3}.");
-}
-else
-{
-    if (num2 > num3) Console.WriteLine($"Max number is {num2}.");
-    else Console.WriteLine($"Max number is {num3}.");
-}
+// if (num1 > num2)
+// {
+//     if (num1 > num3) Console.WriteLine($"Max number is {num1}.");
+//     else Console.WriteLine($"Max number is {num3}.");
+// }
+// else
+// {
+//     if (num2 > num3) Console.WriteLine($"Max number is {num2}.");
+//     else Console.WriteLine($"Max number is {num3}.");
+// }
 
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
+// 4 -> да; -3 -> нет; 7 -> нет
+
+Console.WriteLine("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"{num} IS even.");
+}
+else 
+    Console.WriteLine($"{num} is NOT even.");
+
 
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
