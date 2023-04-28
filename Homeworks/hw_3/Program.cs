363619523e87cc00aebcb6a750a0f1555437eee9
Alexander Lemport <lemportpro@mail.ru>
2023-04-28 20:53:53 +0300

    Task#2 is donw

--- Homeworks/hw_3/Program.cs
+++ Homeworks/hw_3/Program.cs
@@ -11,6 +11,19 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+Console.Write("Input coordinates of point A (x1, y1, z1): ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input coordinates of point B (x2, y2, z2): ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt( Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) );
+
+Console.WriteLine(distance);
 
 
 // Задача 23
@@ -18,10 +31,10 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-Console.Write("Input a number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= num; i++)
-{
-    Console.Write($"{i*i*i} ");
-}
-Console.WriteLine();
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= num; i++)
+// {
+//     Console.Write($"{i*i*i} ");
+// }
+// Console.WriteLine();
