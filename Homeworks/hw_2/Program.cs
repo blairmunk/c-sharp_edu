a11de5f1071772d7c9e4baac8857e8b4f187a434
Alexander Lemport <lemportpro@mail.ru>
2023-04-28 20:20:41 +0300

    Task#3 is done

--- Homeworks/hw_2/Program.cs
+++ Homeworks/hw_2/Program.cs
@@ -18,24 +18,40 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.Write("Input a number: ");
-    int num = Convert.ToInt32(Console.ReadLine());
-    int thirdDigit = -1;
-    while ( num > 99 )
-    {
-        thirdDigit = num % 10;
-        num /= 10;
-    }
+// Console.Write("Input a number: ");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     int thirdDigit = -1;
+//     while ( num > 99 )
+//     {
+//         thirdDigit = num % 10;
+//         num /= 10;
+//     }
 
-    if (thirdDigit != -1)
-    {
-        Console.WriteLine($"The third digit of this number is {thirdDigit}");    
-    }
-    else
-        Console.WriteLine("The third digit doesn't exist");
+//     if (thirdDigit != -1)
+//     {
+//         Console.WriteLine($"The third digit of this number is {thirdDigit}");    
+//     }
+//     else
+//         Console.WriteLine("The third digit doesn't exist");
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("Input number in the range 1..7: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num >= 1 && num <= 5)
+    {
+        Console.WriteLine("It's a workday");    
+    }
+    else if (num <= 7 && num >= 6) 
+    {
+        Console.WriteLine("It's a weekend day");
+    }    
+    else 
+    {
+        Console.WriteLine("You're out of range");
+    }
 
