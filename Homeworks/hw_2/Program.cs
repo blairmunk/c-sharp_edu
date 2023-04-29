9d510e6b16e5d9f404d165af70b5c3747252fa0b
Alexander Lemport <lemportpro@mail.ru>
2023-04-29 17:52:15 +0300

    Second homework is ready to send

--- Homeworks/hw_2/Program.cs
+++ Homeworks/hw_2/Program.cs
@@ -1,47 +1,46 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+// 456 -> 5; 782 -> 8; 918 -> 1
 
-// Console.Write("Input 3-digit number: ");
-//     int num = Convert.ToInt32(Console.ReadLine());
-//     int secondDigit = -1;
-//     while ( num > 99)
-//     {
-//         num /= 10;
-//         secondDigit = num % 10;
-//     }
-//     Console.WriteLine($"The second digit of this number is {secondDigit}");
+void GetSecondDigit(int num)
+{
+    int secondDigit = -1;
+    while ( num > 99)
+    {
+        num /= 10;
+        secondDigit = num % 10;
+    }
+    Console.WriteLine($"The second digit of this number is {secondDigit}");
+}
+Console.Write("Input 3-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+GetSecondDigit(num);
+
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
+// 645 -> 5; 78 -> третьей цифры нет; 32679 -> 6
 
-void GetThirdDigit(int num)
-{
-    int thirdDigit = -1;
-    while ( num > 99 )
-    {
-        thirdDigit = num % 10;
-        num /= 10;
-    }
+// void GetThirdDigit(int num)
+// {
+//     int thirdDigit = -1;
+//     while ( num > 99 )
+//     {
+//         thirdDigit = num % 10;
+//         num /= 10;
+//     }
+//     if (thirdDigit != -1)
+//     {
+//         Console.WriteLine($"The third digit of this number is {thirdDigit}");    
+//     }
+//     else
+//         Console.WriteLine("The third digit doesn't exist");
+// }
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// GetThirdDigit(num);
 
-    if (thirdDigit != -1)
-    {
-        Console.WriteLine($"The third digit of this number is {thirdDigit}");    
-    }
-    else
-        Console.WriteLine("The third digit doesn't exist");
-}
-Console.Write("Input a number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-GetThirdDigit(num);
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-// 6 -> да
-// 7 -> да
-// 1 -> нет
+// 6 -> да; 7 -> да; 1 -> нет
 
 // void WhatsADay(int day)
 // {
@@ -58,7 +57,6 @@ GetThirdDigit(num);
 //         Console.WriteLine("You're out of range");
 //     }
 // }
-
 // Console.Write("Input number in the range 1..7: ");
 // int day = Convert.ToInt32(Console.ReadLine());
 // WhatsADay(day);
