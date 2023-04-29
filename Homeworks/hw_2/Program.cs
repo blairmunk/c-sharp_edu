ac14a198bdf9cd272ce54ea6a84da28cdccd3cea
Alexander Lemport <lemportpro@mail.ru>
2023-04-29 17:26:46 +0300

    Second task solved with metod

--- Homeworks/hw_2/Program.cs
+++ Homeworks/hw_2/Program.cs
@@ -18,45 +18,49 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-// Console.Write("Input a number: ");
-//     int num = Convert.ToInt32(Console.ReadLine());
-//     int thirdDigit = -1;
-//     while ( num > 99 )
-//     {
-//         thirdDigit = num % 10;
-//         num /= 10;
-//     }
+int GetThirdDigit(int num)
+{
+    int thirdDigit = -1;
+    while ( num > 99 )
+    {
+        thirdDigit = num % 10;
+        num /= 10;
+    }
 
-//     if (thirdDigit != -1)
-//     {
-//         Console.WriteLine($"The third digit of this number is {thirdDigit}");    
-//     }
-//     else
-//         Console.WriteLine("The third digit doesn't exist");
+    if (thirdDigit != -1)
+    {
+        Console.WriteLine($"The third digit of this number is {thirdDigit}");    
+    }
+    else
+        Console.WriteLine("The third digit doesn't exist");
+}
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+GetThirdDigit(num);
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да
 // 7 -> да
 // 1 -> нет
 
-void WhatsADay(int day)
-{
-    if (day >= 1 && day <= 5)
-    {
-        Console.WriteLine("It's a workday");    
-    }
-    else if (day <= 7 && day >= 6) 
-    {
-        Console.WriteLine("It's a weekend day");
-    }    
-    else 
-    {
-        Console.WriteLine("You're out of range");
-    }
-}
+// void WhatsADay(int day)
+// {
+//     if (day >= 1 && day <= 5)
+//     {
+//         Console.WriteLine("It's a workday");    
+//     }
+//     else if (day <= 7 && day >= 6) 
+//     {
+//         Console.WriteLine("It's a weekend day");
+//     }    
+//     else 
+//     {
+//         Console.WriteLine("You're out of range");
+//     }
+// }
 
-Console.Write("Input number in the range 1..7: ");
-int day = Convert.ToInt32(Console.ReadLine());
-WhatsADay(day);
+// Console.Write("Input number in the range 1..7: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+// WhatsADay(day);
 
 
