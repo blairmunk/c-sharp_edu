7912b0bff546e93dc857d2512ba2942b092eeae5
Alexander Lemport <lemportpro@mail.ru>
2023-04-29 21:33:30 +0300

    Task #2 (21) rewrotten with metod

--- Homeworks/hw_3/Program.cs
+++ Homeworks/hw_3/Program.cs
@@ -58,19 +58,23 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-// Console.Write("Input coordinates of point A (x1, y1, z1): ");
-// int x1 = Convert.ToInt32(Console.ReadLine());
-// int y1 = Convert.ToInt32(Console.ReadLine());
-// int z1 = Convert.ToInt32(Console.ReadLine());
+double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Round(Math.Sqrt( Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) ), 2);
+}
 
-// Console.Write("Input coordinates of point B (x2, y2, z2): ");
-// int x2 = Convert.ToInt32(Console.ReadLine());
-// int y2 = Convert.ToInt32(Console.ReadLine());
-// int z2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input coordinates of point A (x1, y1, z1): ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
 
-// double distance = Math.Sqrt( Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) );
+Console.Write("Input coordinates of point B (x2, y2, z2): ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
 
-// Console.WriteLine(distance);
+double dist = Distance(x1,y1,z1,x2,y2,z2);
+Console.WriteLine($"Distance between A({x1},{y1},{z1}) and B({x2},{y2},{z2}) points is: {dist}");
 
 
 // Задача 23
@@ -78,16 +82,16 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-int GetCube(int num)
-{
-    for (int i = 1; i <= num; i++)
-    {
-        Console.Write($"{i*i*i} ");
-    }
-    Console.WriteLine();
-    return 0;
-}
+// int GetCube(int num)
+// {
+//     for (int i = 1; i <= num; i++)
+//     {
+//         Console.Write($"{i*i*i} ");
+//     }
+//     Console.WriteLine();
+//     return 0;
+// }
 
-Console.Write("Input a number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-GetCube(num);
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// GetCube(num);
