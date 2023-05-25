59756b1084818064ca179102e07ff8e425e76387
Alexander Lemport <lemportpro@mail.ru>
2023-05-25 22:00:54 +0300

    Third program is done

--- Homeworks/hw_4/Program.cs
+++ Homeworks/hw_4/Program.cs
@@ -30,22 +30,49 @@
 // 82 -> 10
 // 9012 -> 12
 
-int GetDigitsSum(int num)
-{
-    int sum = 0;
-    while (num != 0)
-    {
-        int digit = num % 10;
-        sum += digit;
-        num /= 10;
-    }
-    return sum;
-}
+// int GetDigitsSum(int num)
+// {
+//     int sum = 0;
+//     while (num != 0)
+//     {
+//         int digit = num % 10;
+//         sum += digit;
+//         num /= 10;
+//     }
+//     return sum;
+// }
 
-Console.Write("Input your number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(GetDigitsSum(num));
+// Console.Write("Input your number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(GetDigitsSum(num));
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+int[] CreateArray(int size)
+{
+    int[] newArray = new int[size];
+    Console.WriteLine("Input elements:");
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    return newArray;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine("]");
+}
+
+Console.WriteLine("Input size value of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateArray(size);
+PrintArray(myArray);
