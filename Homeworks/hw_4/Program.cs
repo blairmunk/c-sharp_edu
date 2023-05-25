6626b1093afb324ac1e7e903b2f7a8a3b2cad47c
Alexander Lemport <lemportpro@mail.ru>
2023-05-25 21:24:25 +0300

    Second program is done

--- Homeworks/hw_4/Program.cs
+++ Homeworks/hw_4/Program.cs
@@ -2,27 +2,27 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-int Expon(int bas, int exp)
-{
-    int result = Convert.ToInt32(Math.Pow(bas, exp));
-    return result;
-}
+// int Expon(int bas, int exp)
+// {
+//     int result = Convert.ToInt32(Math.Pow(bas, exp));
+//     return result;
+// }
 
-bool resume = true;
+// bool resume = true;
 
-while (resume == true)
-{
-    Console.Write("Input two numbers: ");
-    int bas = Convert.ToInt32(Console.ReadLine());
-    int exp = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine($"Your result is {Expon(bas, exp)}");
-    Console.WriteLine("Do you want to repeat? (Y/n)");
-    string repeat = Console.ReadLine();
-    if (repeat == "n")
-    {
-        resume = false;
-    } 
-}
+// while (resume == true)
+// {
+//     Console.Write("Input two numbers: ");
+//     int bas = Convert.ToInt32(Console.ReadLine());
+//     int exp = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine($"Your result is {Expon(bas, exp)}");
+//     Console.WriteLine("Do you want to repeat? (Y/n)");
+//     string repeat = Console.ReadLine();
+//     if (repeat == "n")
+//     {
+//         resume = false;
+//     } 
+// }
 
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
@@ -30,7 +30,21 @@ while (resume == true)
 // 82 -> 10
 // 9012 -> 12
 
+int GetDigitsSum(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        int digit = num % 10;
+        sum += digit;
+        num /= 10;
+    }
+    return sum;
+}
 
+Console.Write("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetDigitsSum(num));
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
