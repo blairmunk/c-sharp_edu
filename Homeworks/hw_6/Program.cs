6fc2d8bb5b87b5f033cbf47b27edb9fc200340a6
Alexander Lemport <lemportpro@mail.ru>
2023-06-08 11:05:04 +0300

    Task#43 is done

--- Homeworks/hw_6/Program.cs
+++ Homeworks/hw_6/Program.cs
@@ -2,10 +2,55 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+// int[] CreateManualArray(int size)
+// {
+//     int[] newArray = new int[size];
+//     Console.WriteLine("Input your numbers:");
+//     for (int i = 0; i < size; i++)
+//     {
+//         newArray[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return newArray;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int CountZeros(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] == 0) 
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// Console.WriteLine("How many numbers you want to input?");
+// int M = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateManualArray(M);
+// Console.WriteLine();
+// PrintArray(myArray);
+// Console.WriteLine($"You have {CountZeros(myArray)} zeros");
+
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
 int[] CreateManualArray(int size)
 {
     int[] newArray = new int[size];
-    Console.WriteLine("Input your numbers:");
     for (int i = 0; i < size; i++)
     {
         newArray[i] = Convert.ToInt32(Console.ReadLine());
@@ -13,41 +58,31 @@ int[] CreateManualArray(int size)
     return newArray;
 }
 
-void PrintArray(int[] array)
+double[] FindIntersectionPoint(int[] parameters)
 {
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
+    double[] coordinates = new double[2];
+    int b1 = parameters[0];
+    int k1 = parameters[1];
+    int b2 = parameters[2];
+    int k2 = parameters[3];
+    Console.WriteLine($"Your first function: y = {k1}x + {b1}");
+    Console.WriteLine($"Your second function: y = {k2}x + {b2}");
+    double x = (double) (b2 - b1) / (k1 - k2) ;
+    coordinates[0] = x;
+    double y = k1 * x + b1;
+    coordinates[1] = y;
+    return coordinates;
 }
 
-int CountZeros(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] == 0) 
-        {
-            count++;
-        }
-    }
-    return count;
-}
+Console.WriteLine("You're about to set functions: ");
+Console.WriteLine("y = k1X + b1 and y = k2X + b2 ");
+Console.WriteLine("Input in due order: b1, k1, b2, k2 :");
 
-Console.WriteLine("How many numbers you want to input?");
-int M = Convert.ToInt32(Console.ReadLine());
+int[] parameters = CreateManualArray(4);
+double[] coordinates = FindIntersectionPoint(parameters);
 
-int[] myArray = CreateManualArray(M);
-Console.WriteLine();
-PrintArray(myArray);
+double x = coordinates[0];
+double y = coordinates[1];
 
-Console.WriteLine($"You have {CountZeros(myArray)} zeros");
-
-
-
-
-
-
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Intersection point of your functions is:");
+Console.WriteLine($"({x}; {y})");
