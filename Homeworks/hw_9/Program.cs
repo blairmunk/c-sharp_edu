5ecba4502a3038a3ba584b3b3ff5eed3cf4e5128
Alexander Lemport <lemportpro@mail.ru>
2023-06-12 19:33:30 +0300

    Homework #9 is done

--- Homeworks/hw_9/Program.cs
+++ Homeworks/hw_9/Program.cs
@@ -14,10 +14,12 @@
 // Console.WriteLine("Input N:");
 // int n = Convert.ToInt32(Console.ReadLine());
 // ShowNumbers(n);
+// Console.WriteLine();
+
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
+// M = 4; N = 8 -> 30
 
 // int FindSum(int m, int n)
 // {
